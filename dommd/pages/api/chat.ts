11302f6a1c94cb1141ce77d3418fb1666316940b
Dom <djdomgo@gmail.com>
2023-07-05 03:42:41 +0800

    update prompting

--- dommd/pages/api/chat.ts
+++ dommd/pages/api/chat.ts
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+interface ChatbotResponse {
+  response: string;
+  diagnoses: string[];
+  treatments: string[];
+  doctors: string[];
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
@@ -13,13 +20,43 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // Make a request to the OpenAI GPT API to generate the response
     const response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      'https://api.openai.com/v1/chat/completions',
       {
-        prompt: `Patient: ${message}\nDoctor:`,
-        max_tokens: 100,
-        temperature: 0.7,
-        n: 1,
-        stop: 'Patient:',
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are an AI-powered medical chatbot. You can help provide insights about your symptoms and suggest possible diagnoses, treatments, and recommended doctors.' },
+          { role: 'user', content: `Patient: ${message}` },
+          { role: 'assistant', content: 'assistant: Hello! How can I assist you today? Please describe your symptoms or concerns.' },
+          { role: 'user', content: `Patient: ${message}` },
+
+          { role: 'assistant', content: 'assistant: Sure, I will ask you a few questions to better understand your symptoms.' },
+          { role: 'user', content: `Patient: ${message}` },
+
+          { role: 'assistant', content: 'assistant: What is your age?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+          { role: 'assistant', content: 'assistant: Are you currently taking any medications?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+
+          { role: 'assistant', content: 'assistant: Have you recently traveled to any foreign countries?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+          { role: 'assistant', content: 'assistant: Have you been in contact with someone who has a known illness?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+          { role: 'assistant', content: 'assistant: Have you had any recent changes in your diet or exercise routine?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+
+          { role: 'assistant', content: 'assistant: Are you experiencing any specific pain or discomfort?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+
+          { role: 'assistant', content: 'assistant: Are there any other symptoms or information you would like to share?' },
+          { role: 'user', content: `Patient: ${message}` },
+
+        ],
       },
       {
         headers: {
@@ -30,14 +67,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     );
 
     const { choices } = response.data;
-    const chatbotResponse = choices[0].text.trim();
+    const chatbotResponse = choices[0].message.content.trim();
 
     // Extract diagnoses, treatments, and recommended doctors from the chatbot response
     const diagnoses = extractDiagnoses(chatbotResponse);
     const treatments = extractTreatments(chatbotResponse);
     const doctors = extractDoctors(chatbotResponse);
 
-    return res.status(200).json({ response: chatbotResponse, diagnoses, treatments, doctors });
+    const chatbotResponseWithInfo: ChatbotResponse = {
+      response: chatbotResponse,
+      diagnoses,
+      treatments,
+      doctors,
+    };
+
+    return res.status(200).json(chatbotResponseWithInfo);
   } catch (error) {
     console.error('Error calling OpenAI GPT API:', error);
     return res.status(500).json({ message: 'Error generating chatbot response' });
@@ -46,16 +90,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 // Helper function to extract possible diagnoses from the chatbot response
 const extractDiagnoses = (response: string): string[] => {
-  // Your logic to extract diagnoses from the response goes here
-  // You can use regular expressions, string manipulation, or other techniques
-  // to extract relevant information from the chatbot response
-  const diagnoses = [];
-
-  // Example extraction logic:
-  const regex = /Possible diagnosis: ([^\n]+)/g;
+  const diagnoses: string[] = [];
+  const regex = /Possible diagnosis[s]?:\s*(.*?)\s*(?=Possible treatment[s]?|\n|$)/gs;
   let match;
+
   while ((match = regex.exec(response))) {
-    diagnoses.push(match[1]);
+    const diagnosis = match[1].trim();
+    diagnoses.push(diagnosis);
   }
 
   return diagnoses;
@@ -63,16 +104,13 @@ const extractDiagnoses = (response: string): string[] => {
 
 // Helper function to extract possible treatments from the chatbot response
 const extractTreatments = (response: string): string[] => {
-  // Your logic to extract treatments from the response goes here
-  // Similar to the extractDiagnoses function, you can use the appropriate
-  // techniques to extract relevant information from the chatbot response
-  const treatments = [];
-
-  // Example extraction logic:
-  const regex = /Possible treatment: ([^\n]+)/g;
+  const treatments: string[] = [];
+  const regex = /Possible treatment[s]?:\s*(.*?)\s*(?=Recommended doctor[s]?|\n|$)/gs;
   let match;
+
   while ((match = regex.exec(response))) {
-    treatments.push(match[1]);
+    const treatment = match[1].trim();
+    treatments.push(treatment);
   }
 
   return treatments;
@@ -80,16 +118,13 @@ const extractTreatments = (response: string): string[] => {
 
 // Helper function to extract recommended doctors from the chatbot response
 const extractDoctors = (response: string): string[] => {
-  // Your logic to extract recommended doctors from the response goes here
-  // Similar to the extractDiagnoses and extractTreatments functions,
-  // use the appropriate techniques to extract relevant information
-  const doctors = [];
-
-  // Example extraction logic:
-  const regex = /Recommended doctor: ([^\n]+)/g;
+  const doctors: string[] = [];
+  const regex = /Recommended doctor[s]?:\s*(.*?)\s*(?=\n|$)/gs;
   let match;
+
   while ((match = regex.exec(response))) {
-    doctors.push(match[1]);
+    const doctor = match[1].trim();
+    doctors.push(doctor);
   }
 
   return doctors;
